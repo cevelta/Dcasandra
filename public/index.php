bd8b5708a37479ea5f309b60e988969a4fdb80d0
cevelta <alejandrea.velasquez16c@gmail.com>
2019-09-05 21:15:15 +0100

    Inicio de proyecto Dcasandra

---
+++ public/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+    <title>D'casandra</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
+    <body>
+	<header>
+        <img src="https://placekitten.com/100/100">
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Historía</a></li>
+            <li><a href="">Producto</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+        </nav>
+  </header>
+    <main>
+      <dl>
+        <dt>   <img src="https://placekitten.com/200/200"></dt>
+       <dd> habbnsmbmnbmsb </dd>
+    
+    
+        <dt>   <img src="https://placekitten.com/200/200"></dt>
+       <dd> habbnsmbmnbmsb </dd>
+    
+    
+        <dt>   <img src="https://placekitten.com/200/200"></dt>
+       <dd> habbnsmbmnbmsb </dd>
+    
+
+        <dt>   <img src="https://placekitten.com/200/200"></dt>
+       <dd> habbnsmbmnbmsb </dd>
+ 
+    
+        <dt>   <img src="https://placekitten.com/200/200"></dt>
+       <dd> habbnsmbmnbmsb </dd>
+    </dl>
+      
+    </main>    
+	<footer>
+   <p>Siguenos en nuestras redes</p> 
+   <a href="focebook"></a>
+   <a href="Instagram"></a>
+   <p>Condiciones Servicio</p>
+   <p>Aviso Legal</p>
+  </footer>
+    
+</body>
+	</html>	
