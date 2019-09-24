62c0456288f53852cdb8080f3ff486dbfdd01208
cevelta <alejandrea.velasquez16c@gmail.com>
2019-09-24 06:06:08 +0100

    proyecto final

--- public/index.php
+++ public/index.php
@@ -1,53 +1,11 @@
-<!DOCTYPE html>
-	<html lang="en">
-	<head>
-    <title>D'casandra</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
-    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="js/index.js"></script>
-    <body>
-	<header>
-        <img src="https://placekitten.com/100/100">
-        <nav>
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Historía</a></li>
-            <li><a href="">Producto</a></li>
-            <li><a href="">Login</a></li>
-        </ul>
-        </nav>
-  </header>
-    <main>
-      <dl>
-        <dt>   <img src="https://placekitten.com/200/200"></dt>
-       <dd> habbnsmbmnbmsb </dd>
-    
-    
-        <dt>   <img src="https://placekitten.com/200/200"></dt>
-       <dd> habbnsmbmnbmsb </dd>
-    
-    
-        <dt>   <img src="https://placekitten.com/200/200"></dt>
-       <dd> habbnsmbmnbmsb </dd>
-    
-
-        <dt>   <img src="https://placekitten.com/200/200"></dt>
-       <dd> habbnsmbmnbmsb </dd>
- 
-    
-        <dt>   <img src="https://placekitten.com/200/200"></dt>
-       <dd> habbnsmbmnbmsb </dd>
-    </dl>
-      
-    </main>    
-	<footer>
-   <p>Siguenos en nuestras redes</p> 
-   <a href="focebook"></a>
-   <a href="Instagram"></a>
-   <p>Condiciones Servicio</p>
-   <p>Aviso Legal</p>
-  </footer>
-    
-</body>
-	</html>	
+<?php
+// Activación del reporte de todos los errores y mensajes informativos
+error_reporting(E_ALL);
+// 'display_errors' es el "flag" que activa (1) o desactiva (0) la visualización en pantalla del reporte
+ini_set('display_errors', 1);
+require_once __DIR__.'/../src/bootstrap.php';
+$reflector = new \ReflectionClass(APP);
+$app = $reflector->newInstance();
+if ($app instanceof ApplicationInterface)
+	$app->run();
+exit;
